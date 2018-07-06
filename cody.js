f17ee1298c814bf1d5fe352f46a51f429af61e77
RedCody <cadams1992@yahoo.com>
2018-07-06 18:16:07 -0400

    Add files via upload
    7/6/18

--- cody.js
+++ cody.js
@@ -1,5 +1,14 @@
 
+function promptMe() {
+    var userAdjective = prompt("Please enter your name");
+    alert ( "Hello! "   +  userAdjective);
+}
 
-function fullName() {
-  var x = document.getElementByID("fullName"), value;
+function promptMe2() {
+  var userIllness = prompt("What problem do you have?");
+  alert ("Good News!..Cody can help treat " + userIllness);
+}
+
+function promptMe3() {
+  
 }
